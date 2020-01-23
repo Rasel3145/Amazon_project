de2ad4d63cae5d5240ba993070b9f90485fff101
Rasel3415 <Rasel5777@gmail.com>
2020-01-23 12:38:48 -0500

    Amazon

---
+++ src/AmazonHomepage/AmazonTestCase.java
@@ -0,0 +1,69 @@
+package AmazonHomepage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonTestCase {
+    static String url = "https://www.amazon.com/";
+
+    public static void main(String[] args) throws InterruptedException {
+        // For Chrome Browser
+        System.setProperty("webdriver.chrome.driver", "BrowserDriver/chromedriver");
+        // create a object for chromeDriver
+        WebDriver driver = new ChromeDriver();
+        //driver.get(url);
+        driver.navigate().to(url);
+        //Will maximize the window
+        driver.manage().window().maximize();
+        //find the element and enter keys to be entered
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Samsung");
+        //Will delay next phase
+        Thread.sleep(5000);
+        //find element that will click on the search button
+        driver.findElement(By.cssSelector("#nav-search > form > div.nav-right > div > input")).click();
+        //will relocate to previous page
+        driver.navigate().back();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // will close the Browser which is active/current
+        driver.close();
+        // close all tab open/all the windows will close
+        //driver.quit();
+    }
+}
